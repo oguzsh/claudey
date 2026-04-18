f08125025b85147009142b972f877c8570677307
Oguzhan Ince <oguzhan824@gmail.com>
2026-04-19 01:50:51 +0300

    feat(rust): port check-console-log hook

--- src/hooks/mod.rs
+++ src/hooks/mod.rs
@@ -7,6 +7,7 @@
 
 use serde_json::Value;
 
+mod check_console_log;
 mod post_edit_console_warn;
 mod post_edit_format;
 mod post_edit_typecheck;
@@ -14,6 +15,7 @@ mod pre_compact;
 mod session_end;
 mod session_start;
 mod suggest_compact;
+pub use check_console_log::check_console_log;
 pub use post_edit_console_warn::post_edit_console_warn;
 pub use post_edit_format::post_edit_format;
 pub use post_edit_typecheck::post_edit_typecheck;
@@ -21,10 +23,6 @@ pub use pre_compact::pre_compact;
 pub use session_end::session_end;
 pub use session_start::session_start;
 pub use suggest_compact::suggest_compact;
-
-pub fn check_console_log(_raw: Vec<u8>) {
-    eprintln!("check-console-log stub");
-}
 
 pub fn evaluate_session(_input: Value, _plugin_root: String) {
     eprintln!("evaluate-session stub");
