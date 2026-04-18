17f8b3dbb992d4c80319ed52495b2a2a1a9e1dc3
Oguzhan Ince <oguzhan824@gmail.com>
2026-04-19 01:51:50 +0300

    feat(rust): port evaluate-session hook

--- src/hooks/mod.rs
+++ src/hooks/mod.rs
@@ -8,6 +8,7 @@
 use serde_json::Value;
 
 mod check_console_log;
+mod evaluate_session;
 mod post_edit_console_warn;
 mod post_edit_format;
 mod post_edit_typecheck;
@@ -16,6 +17,7 @@ mod session_end;
 mod session_start;
 mod suggest_compact;
 pub use check_console_log::check_console_log;
+pub use evaluate_session::evaluate_session;
 pub use post_edit_console_warn::post_edit_console_warn;
 pub use post_edit_format::post_edit_format;
 pub use post_edit_typecheck::post_edit_typecheck;
@@ -23,10 +25,6 @@ pub use pre_compact::pre_compact;
 pub use session_end::session_end;
 pub use session_start::session_start;
 pub use suggest_compact::suggest_compact;
-
-pub fn evaluate_session(_input: Value, _plugin_root: String) {
-    eprintln!("evaluate-session stub");
-}
 
 pub fn git_push_reminder(_input: Value, _raw: Vec<u8>) {
     eprintln!("git-push-reminder stub");
